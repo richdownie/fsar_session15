c2aff2fb397c425ba9a1db894e3eeeb0df7c9b0a
Rich Downie <rich@blackboxd.com>
2018-12-22 13:56:43 -0500

    initial commit

---
+++ features/support/hooks.rb
@@ -0,0 +1,13 @@
+require 'selenium-webdriver'
+require 'webdrivers'
+require 'cucumber'
+require 'rspec'
+require 'pry'
+require 'httparty'
+require_relative '../page_objects/widgets_index_page'
+
+$caps = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: [ "--headless" ]})
+
+Before do
+  @widgets_index = WidgetsIndex.new(@browser, @domain)
+end
